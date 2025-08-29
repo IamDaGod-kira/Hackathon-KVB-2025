925423732a77ff3c923aaae336392864de598928
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-08-29 10:52:39 +0530

    first work from school

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/home';
+import React, {useState} from 'react';
+import Main from './ctrl.jsx'
+    const [isRendered, setisRendered] = useState(false)
+
+const toggle = () => {
+    // Check if all credentials are provided
+    if (email && username && password && uniqueId) {
+      setisRendered(true);  // If credentials are present, show the Main component
+    } else {
+      alert('Please fill all the fields.');  // If not, show an alert
+      window.location.reload();  // Reload the page to reset the form
+    }
+  };
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    <button  type ="submit" onclick = {toggle}>Login to go to school website  </button>
+            {isRendered ? <Main /> : <Home/>}
+            </>
   )
 }
 
-export default App
+export default App;
