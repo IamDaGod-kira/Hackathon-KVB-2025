a3b337f7336980b3c5f23d7e9e714adef239e8f6
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-08-31 07:10:28 +0530

    added Enrollment page

--- src/components/sub-parts/enroll.jsx
+++ src/components/sub-parts/enroll.jsx
@@ -6,37 +6,37 @@ export default function Enroll () {
         <h2>Student Enrollment Form</h2>
         <form action="#" method="post">
       <label htmlFor="fullname">Full Name of Student</label>
-      <input type="text" id="fullname" name="fullname" placeholder="Enter full name of student" required />
+      <input className="input" type="text" id="fullname" name="fullname" placeholder="Enter full name of student" required />
       <label htmlFor="fullname">Name of Father</label>
-      <input type="text" id="fullname" name="fullname" placeholder="Enter name of father" required/>
+      <input className="input" type="text" id="fullname" name="fullname" placeholder="Enter name of father" required/>
       <label htmlFor="fullname">Name of Mother</label>
-      <input type="text" id="fullname" name="fullname" placeholder="Enter name of mother" required/>
+      <input className="input" type="text" id="fullname" name="fullname" placeholder="Enter name of mother" required/>
 
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" placeholder="Enter email" required/>
+      <input className="input" type="email" id="email" name="email" placeholder="Enter email" required/>
       <label htmlFor="email">Unique ID</label>
-      <input type="email" id="email" name="email" placeholder="Enter Unique ID" required/>
+      <input className="input" type="email" id="email" name="email" placeholder="Enter Unique ID" required/>
 
 
       <label htmlFor="phone">Phone Number</label>
-      <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" required/>
+      <input className="input" type="tel" id="phone" name="phone" placeholder="Enter your phone number" required/>
       <label htmlFor="phone">Alternative Phone Number</label>
-      <input type="tel" id="phone" name="phone" placeholder="Enter your alternative phone number" required/>
+      <input className="input" type="tel" id="phone" name="phone" placeholder="Enter your alternative phone number" required/>
 
 
       <label>Gender</label>
       <div className="radio-group">
-        <label><input type="radio" name="gender" value="male" required/> Male</label>
-        <label><input type="radio" name="gender" value="female"/> Female</label>
-        <label><input type="radio" name="gender" value="other"/> Other</label>
+        <label><input className="input" type="radio" name="gender" value="male" required/> Male</label>
+        <label><input className="input" type="radio" name="gender" value="female"/> Female</label>
+        <label><input className="input" type="radio" name="gender" value="other"/> Other</label>
       </div> 
       <label>Physically Disabled</label>
       <div className="radio-group">
-        <label><input type="radio" name="handicapped" value="yes" required/> Yes</label>
+        <label><input className="input" type="radio" name="handicapped" value="yes" required/> Yes</label>
         <label><input type="radio" name="handicapped" value="no"/> No</label>
       </div> 
       <label htmlFor="className">Select Class</label>
-      <select id="className" name="className" required onchange="toggleCourse()">
+      <select className="select" name="className" required onchange="toggleCourse()">
         <option value="">-- Choose Class --</option>
         <option value="1">Balvatika I</option>
         <option value="1">Balvatika II</option>
@@ -57,7 +57,7 @@ export default function Enroll () {
 
       <div id="courseSection" style="display:none;">
         <label htmlFor="course">Select Course</label>
-        <select id="course" name="course">
+        <select className="select" id="course" name="course">
           <option value="">-- Choose a Stream --</option>
           <option value="science">Science(Computer Science)</option>
           <option value="science">Science(Bilology)</option>
@@ -66,7 +66,7 @@ export default function Enroll () {
         </select>
       </div>
       <label htmlFor="address">Address</label>
-      <textarea id="address" name="address" rows="4" placeholder="Enter address" required></textarea>
+      <textarea class="textarea" id="address" name="address" rows="4" placeholder="Enter address" required></textarea>
 
       <button className="button4" type="submit">Enroll Now</button>
     </form>   
